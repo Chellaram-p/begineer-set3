ea061eab309d4d8220a529063e0281135b8f5425
Chellaram-p <37235951+Chellaram-p@users.noreply.github.com>
2018-06-21 22:12:22 +0530

    Create EX21.py

---
+++ EX21.py
@@ -0,0 +1,7 @@
+n=int(input("Enetr the value of number of values"))
+a=int(input("Enter the value of number of series"))
+d=int(input("Enter the value of difference"))
+l=(n*d)+a-1
+sum=0
+sum=((a+l)*n)/2
+print("sum:",int(sum))
